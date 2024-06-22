a53c986bb5d5439f54fcea2939374d1ce24c2146
PejeDev <hi@pejedev.xyz>
2024-06-21 23:55:07 -0500

    improved docs workflow

--- docs/astro.config.mjs
+++ docs/astro.config.mjs
@@ -1,10 +1,25 @@
 import { defineConfig } from "astro/config";
 import starlight from "@astrojs/starlight";
+import starlightBlog from "starlight-blog";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://codewhiskers.pejedev.xyz",
   integrations: [
     starlight({
+      plugins: [
+        starlightBlog({
+          authors: {
+            pejedev: {
+              name: "PejeDev",
+              title: "King of Mice",
+              picture:
+                "https://pbs.twimg.com/profile_images/1707612485291827200/wk3_fhMU_400x400.jpg",
+              url: "https://pejedev.xyz",
+            },
+          },
+        }),
+      ],
       title: "Code🐭Whiskers",
       social: {
         github: "https://github.com/PejeDev/CodeWhiskers",
---
+++ docs/src/content/docs/blog/my-first-blog-post.md
@@ -0,0 +1,10 @@
+---
+title: My first blog post
+date: 2024-06-21
+authors:
+  - pejedev
+---
+
+## Hello
+
+Hello world!
