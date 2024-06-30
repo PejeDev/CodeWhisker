b48ed5aeca418d330c83894d80993d501623e854
PejeDev <hi@pejedev.xyz>
2024-06-30 16:00:19 -0500

    🔨chore: updated docs command

--- packages/executor/src/lib/services/whiskers-docs.ts
+++ packages/executor/src/lib/services/whiskers-docs.ts
@@ -12,5 +12,5 @@ export async function runWhiskersDocs(
   logger.info(`🚀 initializing ${values.service}`)
   const excCommand = values.dev ? `${context.commands}:dev` : context.commands
   logger.info(`🚀 running ${excCommand} on ${values.service}`)
-  await $`bun --filter '${values.service}' ${excCommand} `
+  await $`bun --cwd ${`${context.projectFolder}/apps/${values.service}`} ${excCommand} `
 }
